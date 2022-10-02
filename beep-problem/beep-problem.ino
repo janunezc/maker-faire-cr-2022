80c4b837e3a1d8930440b60c4dd5d42485bfd20e
jn <jose.a.nunez@gmail.com>
2022-10-02 08:47:50 -0600

    v1

---
+++ beep-problem/beep-problem.ino
@@ -0,0 +1,30 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Bienvenido al problema del Beep");
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+long timeInit = millis();
+void loop() {
+  int segundos =  (millis() - timeInit) / 1000;
+
+  printSeconds(segundos);
+  if (segundos > 0 && segundos % 10 == 0) {
+    beep(250, 250, 4);
+  }
+
+}
+
+void printSeconds(int segundos){
+  Serial.print("Segundos: ");
+  Serial.println(segundos);
+}
+
+void beep(int tiempoDeSonido, int tiempoDePausa, int veces) {
+  for (int i = 0; i < veces; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(tiempoDeSonido);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(tiempoDePausa);
+  }
+}
